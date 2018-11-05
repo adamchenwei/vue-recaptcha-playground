6c7c4faffa1e2639d5a7351a8579f975d3875e11
Adam Chen Wei <adamchenwei@gmail.com>
2018-11-05 13:51:55 -0500

    feat(invisible recaptcha): basic home page submit button setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-recaptcha sitekey="6Lfe33gUAAAAAMCuDwRfhSUV4sGkqGDaGrKqjkmZ">
+      <button type="submit">Submit</button>
+    </vue-recaptcha>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import VueRecaptcha from 'vue-recaptcha';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    VueRecaptcha,
   }
 }
 </script>
